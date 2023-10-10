40d006bc03bb6498b7a830748a4375b9b12523bf
patrickonesiah <patrickonesiah@gmail.com>
2023-10-10 14:13:38 +1100

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (names) => names.slice(0, 2)
+
+const returnLastTwoDrivers = (names) => names.slice(2, 4)
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (value) => (fare) => value * fare;
+
+const fareDoubler = createFareMultiplier(2); 
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers,selectScuberDrivers) => selectScuberDrivers(arrayOfDrivers); // Code your solution in this file!
